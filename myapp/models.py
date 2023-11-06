54207a0bcee240327afd7fd02944533564f5acaf
Sanjananaik4 <147262844+Sanjananaik4@users.noreply.github.com>
2023-11-07 01:02:55 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+  item_name = models.CharFields(max_length=255)
+  description = models.TextField()
+  location_lost = models.CharField(max_length=255)
+  date_created = models.DateTimeField(auto_now_add=True)
