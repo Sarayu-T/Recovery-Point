6a4c9ba979508554d3b17d197b3fb9ea2995646c
Sanjananaik4 <147262844+Sanjananaik4@users.noreply.github.com>
2023-11-07 01:00:03 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from myapp.models import Item
 # Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ('item_name', 'description', 'location_lost', 'date_reported')
+  list_filter = ('item_name', 'location_lost')
+  search_fields = ('item_name', 'description', 'location_lost')
