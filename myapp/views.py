875a1197e7532c0ece8e4e8111a2704398db3329
Sanjananaik4 <147262844+Sanjananaik4@users.noreply.github.com>
2023-11-07 01:07:43 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Item
+import pymysql as sql
 
 # Create your views here.
+def search(request):
+  query = request.GET.get('search')
+  results = Item.objects.filter(item_name__icontains=query) | Item.objects.filter(location_lost__icontains=query)
+
+return render(request, 'searching.html')
