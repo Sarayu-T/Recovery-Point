97a13532138da52337d770d2af862176b52fcc70
Sanjananaik4 <147262844+Sanjananaik4@users.noreply.github.com>
2023-11-17 09:03:43 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from myapp.models import Item
+from myapp.models import LostItemDetails
 # Register your models here.
 
+'''
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
   list_display = ('item_name', 'description', 'location_lost', 'date_reported')
   list_filter = ('item_name', 'location_lost')
   search_fields = ('item_name', 'description', 'location_lost')
+'''
